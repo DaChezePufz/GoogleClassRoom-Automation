b84a31e5b6693c9a6ed75bb115140e58ed202123
Jake Warner <70581354+DaChezePufz@users.noreply.github.com>
2021-01-04 18:25:48 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+#EDIT THIS FIRST TO CONFIGURE EVERYTHING
+browser_chrome = "False"
+browser_firefox = "True"
+
+#school_email = "" #your school email adress
+school_email_passord = "" #your school email passord, leave blank if you use realsmart
+
+
+realsmart = "True" #true if your school uses realsmart, false if not
+
+#IF YOU DONT USE REALSMART, IGNORE BELOW
+realsmart_school = "" #the school as displayed by realsmart
+realsmart_username = "" #the username you use for realsmart
+realsmart_user_passoword = "" # your passord
